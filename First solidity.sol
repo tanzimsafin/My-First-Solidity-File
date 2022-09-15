4d5e1ad6341d50d712f9a30a7f923aad60d5c38e
tanzimsafin <106369796+tanzimsafin@users.noreply.github.com>
2022-09-15 16:19:57 +0600

    Hello world

---
+++ First solidity.sol
@@ -0,0 +1,10 @@
+ // SPDX-License-Identifier:MIT
+ pragma solidity 0.8.7;
+ contract HellowWorld{
+     string public mystring='hello world';
+     uint public myuint=20;
+     int public myint=-1829;
+     bool public mybool=true;
+     int public minint=type(int).min;
+     int public maxint=type(int).max;
+ }
